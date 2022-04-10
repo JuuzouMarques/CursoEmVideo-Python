5f8d5e6d41422659a2429cd0f2568ef93e1e647c
Juuzou Marques <miltonneto2002@gmail.com>
2022-04-10 10:09:52 -0300

    Resolução de exercícios

---
+++ ex091.py
@@ -0,0 +1,10 @@
+from random import randint
+from time import sleep
+
+jogadores = dict()
+print('Valores sorteados:')
+for i in range(1, 5):
+    jogadores[f'{i}º'] = randint(1, 6)
+    print(f'O jogador {i} tirou {jogadores[f"{i}º"]}')
+    sleep(1)
+
