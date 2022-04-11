cc1e465a7f64366f98e127809e6da14a08ffc12b
Juuzou Marques <miltonneto2002@gmail.com>
2022-04-11 18:25:20 -0300

    Correção de exercícios

--- ex093.py
+++ ex093.py
@@ -14,3 +14,4 @@ print('-=' * 30)
 print(f'O jogador {jogador["nome"]} jogou {jogador["partidas"]} partidas')
 for i in range(jogador['partidas']):
     print(f'    => Na partida {i+1}, fez {jogador["gols"][i]} gols.')
+print(f'Foi um total de {jogador["totalGols"]} gols')
