ad915e55151c350d28f88d69f0cde0bb8bbff012
Juuzou Marques <miltonneto2002@gmail.com>
2022-04-13 22:41:38 -0300

    Resolução exercício 108

---
+++ ex108/moeda.py
@@ -0,0 +1,22 @@
+def aumentar(preco=0, taxa=0):
+    res = preco + preco * taxa / 100
+    return res
+
+
+def diminuir(preco=0, taxa=0):
+    res = preco - preco * taxa / 100
+    return res
+
+
+def dobro(preco=0):
+    res = 2 * preco
+    return res
+
+
+def metade(preco=0):
+    res = preco / 2
+    return res
+
+
+def moeda(preco=0, moeda='R$'):
+    res = f'{moeda}{preco:.2f}'.replace('.', ',')
