a01b41d109194b972ccfa92d3eaf5344e789b0f1
Juuzou Marques <miltonneto2002@gmail.com>
2022-04-14 11:43:36 -0300

    Correção exercício 110

--- ex109/teste.py
+++ ex109/teste.py
@@ -1,7 +1,7 @@
 from ex109 import moeda
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.dobro(p))}')
-print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}')
-print(f'Diminuindo 10%, temos {moeda.moeda(moeda.diminuir(p, 10))}')
+print(f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.metade(p, True)}')
+print(f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.dobro(p, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p, 10, True)}')
+print(f'Diminuindo 10%, temos {moeda.diminuir(p, 10, True)}')
