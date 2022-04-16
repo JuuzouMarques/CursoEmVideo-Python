8fdeae09f69d4047b6532a4759af115beedb8545
Juuzou Marques <miltonneto2002@gmail.com>
2022-04-15 21:51:30 -0300

    Correção exercício 115

--- ex115 correção/sistema.py
+++ ex115 correção/sistema.py
@@ -1,14 +1,23 @@
 from lib.interface import *
+from lib.arquivo import *
+
+arq = 'ex115 correção\\dados.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 # PROGRAMA PRINCIPAL
 while True:
     resposta = menu(['Listar Pessoas', 'Cadastrar Pessoas', 'Sair do Sistema'])
     if resposta == 1:
-        print('OPÇÃO 1')
+        lerArquivo(arq)
     elif resposta == 2:
-        print('OPÇÃO 2')
+        cabecalho('NOVO CADASTRO')
+        nome = input('Nome: ')
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
-        print('Saindo do Sistema! Até logo!')
+        cabecalho('Saindo do Sistema! Até logo!')
         break
     else:
-        print('ESCOLHA UMA OPÇÃO VÁLIDA!')
+        print('ERRO! Selecione uma opção válida!')
